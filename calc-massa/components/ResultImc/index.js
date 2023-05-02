eb1c9137a7cd94604336cbbe50a6c65c27733a14
Eudis Alves <deleudis@gmail.com>
2023-05-02 18:10:57 -0300

    adicionando função de compartilhamento

--- calc-massa/components/ResultImc/index.js
+++ calc-massa/components/ResultImc/index.js
@@ -1,12 +1,29 @@
 import React from "react";
 import { StyleSheet } from "react-native";
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity, Share } from "react-native";
+import styles from "../Form/style";
 
 export default function ResultImc(props) {
+
+    const onShere = async () => {
+        const result = await Share.share({
+           massege:"Meu imc hoje é:" +props.resltImc,
+        })
+    }
+
     return(
-        <View>
-            <Text>{props.messageResultImc}</Text>
-            <Text>{props.resultImc}</Text>
+        <View style={styles.conTextImc}>
+            <View style={styles.boxShareButton}>
+                {props.resultImc != null ?
+                <TouchableOpacity style={styles.shared}>
+                    <Text style={styles.sharedText}>Share</Text>
+                </TouchableOpacity>
+                :
+                <View />
+                }
+            </View>
+            <Text style={styles.TitleResultImc}>{props.messageResultImc}</Text>
+            <Text style={styles.resltImc}>{props.resultImc}</Text>
         </View>
     )
 }
