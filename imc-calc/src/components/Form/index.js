d7a932a510d2c303bd80fed2e4b29d4283987d27
Eudis Alves <deleudis@gmail.com>
2023-05-18 20:47:03 -0300

    finalizando logica do ResultImc

--- imc-calc/src/components/Form/index.js
+++ imc-calc/src/components/Form/index.js
@@ -1,22 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, TextInput, Button} from "react-native";
 import ResultImc from "../ResultImc";
 
 export default function Form() {
+
+    const [height, setHeight] = useState(null)
+    const [weight, setWeight] = useState(null)
+    const [messageImc, setMessageImc] = useState("preencha o peso e a altura")
+    const [imc, setImc] = useState(null)
+    const [textButton, setTextButton] = useState("Calcular")
+
+    function imcCalculator() {//formula para calcular o imc peso dividido pela altura veses altura
+        return setImc((weight/(height * height)).toFixed(2))
+    }
+
+    function validationImc() {
+       if (weight != null && height != null) {
+        imcCalculator()
+        setHeight(null)
+        setWeight(null)
+        setMessageImc("seu imc é igual:")
+        setTextButton("Calcular novamente")
+        return
+       } 
+       setImc(null)
+       setTextButton("Calcular")
+       setMessageImc("preencha o peso altura!")
+    }
+
     return (
         <View>
             <View>
                 <Text>Altura</Text>
                 <TextInput
+                onChangeText={setHeight}
+                value={height}
                 placeholder="Ex. 1.50"
                 keyboardType="numeric"
                 />
                 <Text>Peso</Text>
                 <TextInput
+                onChangeText={setWeight}
+                value={weight}
                 placeholder="Ex. 50.355"
                 keyboardType="numeric"
                 />
-                <Button title="Calcular IMC"/>
+                <Button 
+                onPress={() => validationImc()}
+                title={textButton}
+                />
             </View>
             <ResultImc messageResultImc={messageImc} resultImc={imc}/>
             <View></View>
