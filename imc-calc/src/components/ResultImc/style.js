6627633dcfd9e6caba6efd102becc8efb7bfce4e
Eudis Alves <deleudis@gmail.com>
2023-05-18 22:22:04 -0300

    criando style do resultImc

--- imc-calc/src/components/ResultImc/style.js
+++ imc-calc/src/components/ResultImc/style.js
@@ -1,8 +1,23 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create ({
-    Title: {
-
+    resultImc: {
+        flex: 1,
+        marginTop: 15,
+        paddingTop: 60,
+        borderRadius: 50,
+        alignItems: "center",
+        width: "100%",
+    },
+    numberImc: {
+        fontSize: 68,
+        color: "#FF0043",
+        fontWeight: "bold"
+    },
+    information: {
+        fontSize: 18,
+        color: "#FF0043",
+        fontWeight: "bold"
     },
 });
 
