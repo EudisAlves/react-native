cb5b6aa8fd0a219874d438a5c1652b5b1648d3b9
Eudis Alves <deleudis@gmail.com>
2023-08-23 20:05:09 -0300

    criando styles externos

---
+++ PlayerMusic/src/screens/load/Loading.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import styles from './style';
+
+export function Loading() {
+  return (
+    <View style={styles.container}>
+        <Text>Tela 2</Text>
+    </View>
+  );
+}
