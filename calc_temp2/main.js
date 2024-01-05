a50fc1108d7847395b3599d5f575f6ffd6917ce4
Eudis Alves <deleudis@gmail.com>
2024-01-05 07:12:41 -0300

    finalisando estrutura e começando a logica do js

---
+++ calc_temp2/main.js
@@ -0,0 +1,14 @@
+function calcularTempero(pesoPrato, tipoPrato) {
+
+  const temperos = {
+
+    'arroz-branco': {
+      'Arroz branco': pesoPrato * 0.461,
+      Alho: pesoPrato * 0.005,
+      Agisal: pesoPrato * 0.011,
+      Oleó: pesoPrato * 0.069,
+      'Folha de louro': pesoPrato * 3.000
+    }
+
+}
+}
